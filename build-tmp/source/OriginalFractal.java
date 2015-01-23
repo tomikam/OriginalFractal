70363e6e3a498ee3b58dc27b08283e9c1a0d0153
unknown <thwhite@697-334-Linhir.sf.k12.internal>
2015-01-23 10:47:39 -0800

    First commit, half of base case

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
 public void setup() {
 	size(1600, 500);
 }
@@ -66,4 +82,13 @@ public void fractal(int x, int y, int len) {
 		popMatrix();
 		popMatrix();
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
